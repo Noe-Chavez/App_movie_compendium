c192ca704bff5a3e7c26f867cda2d98ef57d6aaf
Melchiah Razziel Reaven <razzielreaven@gmail.com>
2021-02-09 21:38:20 -0600

    added view banding.

--- app/src/main/java/mx/com/disoftware/movieconpendium/MovieDetailFragment.kt
+++ app/src/main/java/mx/com/disoftware/movieconpendium/MovieDetailFragment.kt
@@ -5,9 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import mx.com.disoftware.movieconpendium.databinding.FragmentMovieDetailBinding
 
 class MovieDetailFragment : Fragment(R.layout.fragment_movie_detail) {
+
+    private lateinit var binding: FragmentMovieDetailBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentMovieDetailBinding.bind(view) // también está seteado en el xml, pero se coloca en código como ejemplo.
+
     }
+
 }
--- app/src/main/java/mx/com/disoftware/movieconpendium/MovieFragment.kt
+++ app/src/main/java/mx/com/disoftware/movieconpendium/MovieFragment.kt
@@ -5,9 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import mx.com.disoftware.movieconpendium.databinding.FragmentMovieBinding
 
 class MovieFragment : Fragment(R.layout.fragment_movie) {
+
+    /**
+     *  Obs. onViewCreated se visualiza que se pasa en el constructor de este fragment el recurso R.layout.fragment_movie.
+     *  y en el método onViewCreated se puede acceder a la vista (R.layout.fragment_movie) con el parámetro view, de esta
+     *  manera ya no es necesario onCreateView.
+     */
+
+    private lateinit var binding: FragmentMovieBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentMovieBinding.bind(view)
+        /* acceder a los recuros definidos en la vista R.layout.fragment_movie.
+           NOTA, los nomnbres de los recursos los realiza con notación camel case, no como originalmnete se colocaron en el xml.
+           del fragmento (R.layout.fragment_movie).
+         */
+        binding.progressBar.visibility = View.GONE
+
     }
+
 }
