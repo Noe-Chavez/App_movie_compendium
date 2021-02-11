4f541e5fa9d1e7eaf53d594286ce587eff6a32a4
Melchiah Razziel Reaven <razzielreaven@gmail.com>
2021-02-10 18:34:44 -0600

    data class Movie.kt

--- app/src/main/java/mx/com/disoftware/movieconpendium/data/model/Movie.kt
+++ app/src/main/java/mx/com/disoftware/movieconpendium/data/model/Movie.kt
@@ -1,4 +1,20 @@
 package mx.com.disoftware.movieconpendium.data.model
 
-data class Movie(val id: Int = -1) {
-}
+data class Movie(
+        val id: Int = -1,
+        val adult: Boolean = false,
+        val genre_ids: List<Int> = listOf(),
+        val backdrop_path: String = "not path",
+        val original_title: String = "no title established",
+        val original_language: String = "no language established",
+        val overview: String = "no overview established",
+        val popularity: Double = -0.0,
+        val poster_path: String = "no image",
+        val release_date: String = "0000-00-00",
+        val title: String = "not name",
+        val video: Boolean = false,
+        val vote_average: Double = -1.0,
+        val vote_count: Int = -1
+)
+
+data class MovieList(val results: List<Movie> = listOf())
