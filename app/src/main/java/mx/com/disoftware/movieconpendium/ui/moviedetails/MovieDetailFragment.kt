0433e136b99ca361a8febe4b9bada70d52359d47
Noe Chávez <noe_fam_chavez@yahoo.com.mx>
2021-02-16 20:44:16 -0600

    preset Errors in details

--- app/src/main/java/mx/com/disoftware/movieconpendium/ui/moviedetails/MovieDetailFragment.kt
+++ app/src/main/java/mx/com/disoftware/movieconpendium/ui/moviedetails/MovieDetailFragment.kt
@@ -3,18 +3,28 @@ package mx.com.disoftware.movieconpendium.ui.moviedetails
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import mx.com.disoftware.movieconpendium.R
 import mx.com.disoftware.movieconpendium.databinding.FragmentMovieDetailBinding
 
 class MovieDetailFragment : Fragment(R.layout.fragment_movie_detail) {
 
     private lateinit var binding: FragmentMovieDetailBinding
+    private val args by navArgs<MovieDetailFragmentArgs>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding = FragmentMovieDetailBinding.bind(view) // también está seteado en el xml, pero se coloca en código como ejemplo.
 
+        Glide.with(requireContext()).load("https://image.tmdb.org/t/p/w500/${args.posterImageUrl}").centerCrop().into(binding.imgMovie)
+        Glide.with(requireContext()).load("https://image.tmdb.org/t/p/w500/${args.backgroundImageUrl}").centerCrop().into(binding.imgBackground)
+        binding.txtDescription.text = args.overview
+        binding.txtMovieTitle.text = args.title
+        binding.txtLanguage.text = "Language ${args.language}"
+        binding.txtRating.text = "${args.voteAvarage} (${args.voteCount} Reviews)"
+        binding.txtReleased.text = "Released ${args.releaseDate}"
     }
 
 }
