f5ee06ea1305e0b07617888f066171a7ea23eb9c
Melchiah <noe_fam_chavez@yahoo.com.mx>
2021-05-10 02:17:04 -0500

    validate internet connection.

---
+++ app/src/main/java/mx/com/disoftware/movieconpendium/core/InternetChack.kt
@@ -0,0 +1,21 @@
+package mx.com.disoftware.movieconpendium.core
+
+import kotlinx.coroutines.coroutineScope
+import java.io.IOException
+import java.net.InetSocketAddress
+import java.net.Socket
+
+object InternetChack {
+    suspend fun isNetworkAvailable() = coroutineScope {
+        return@coroutineScope try {
+            // Vadar si hay internet (emulamos un ping a 8.8.8.8)
+            val sock = Socket()
+            val socketAddress = InetSocketAddress("8.8.8.8", 53)
+            sock.connect(socketAddress, 2000)
+            sock.close()
+            true
+        } catch (e: IOException) {
+            false
+        }
+    }
+}
--- app/src/main/java/mx/com/disoftware/movieconpendium/repository/MovieRepositoryImpl.kt
+++ app/src/main/java/mx/com/disoftware/movieconpendium/repository/MovieRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package mx.com.disoftware.movieconpendium.repository
 
+import mx.com.disoftware.movieconpendium.core.InternetChack
 import mx.com.disoftware.movieconpendium.ui.data.local.LocalMovieDataSource
 import mx.com.disoftware.movieconpendium.ui.data.model.MovieList
 import mx.com.disoftware.movieconpendium.ui.data.model.toMovieEntity
@@ -11,24 +12,36 @@ class MovieRepositoryImpl(
 ) : MovieRepository {
 
     override suspend fun getUpcomingMovies(): MovieList {
-        dataSourceRemote.getUpcomingMovies().results.forEach { movie ->
-            dataSourceLocal.saveMovie(movie.toMovieEntity("upcoming"))
+        return if (InternetChack.isNetworkAvailable()) {
+            dataSourceRemote.getUpcomingMovies().results.forEach { movie ->
+                dataSourceLocal.saveMovie(movie.toMovieEntity("upcoming"))
+            }
+            dataSourceLocal.getUpcomingMovies()
+        } else {
+            dataSourceLocal.getUpcomingMovies()
         }
-        return dataSourceLocal.getUpcomingMovies()
+
     }
 
     override suspend fun getTopRatedMovies(): MovieList {
-        dataSourceRemote.getTopRatedMovies().results.forEach { movie ->
-            dataSourceLocal.saveMovie(movie.toMovieEntity("toprated"))
+        return if (InternetChack.isNetworkAvailable()) {
+            dataSourceRemote.getTopRatedMovies().results.forEach { movie ->
+                dataSourceLocal.saveMovie(movie.toMovieEntity("toprated"))
+            }
+            dataSourceLocal.getTopRatedMovies()
+        } else {
+            dataSourceLocal.getTopRatedMovies()
         }
-        return dataSourceLocal.getTopRatedMovies()
+
     }
 
     override suspend fun getPopularMovies(): MovieList {
-        dataSourceRemote.getPopularMovies().results.forEach { movie ->
-            dataSourceLocal.saveMovie(movie.toMovieEntity("popular"))
-        }
-        return dataSourceLocal.getPopularMovies()
+        return if (InternetChack.isNetworkAvailable()) {
+            dataSourceRemote.getPopularMovies().results.forEach { movie ->
+                dataSourceLocal.saveMovie(movie.toMovieEntity("popular"))
+            }
+            dataSourceLocal.getPopularMovies()
+        } else
+            dataSourceLocal.getPopularMovies()
     }
-
 }
